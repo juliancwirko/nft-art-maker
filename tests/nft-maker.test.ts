d84169624fa26337355ae6e21927d0adbb95284d
Dominic Garms <djgarms@gmail.com>
2022-05-09 14:45:00 +0800

    clean log of test

--- tests/nft-maker.test.ts
+++ tests/nft-maker.test.ts
@@ -9,9 +9,7 @@ describe('nft-maker tests', () => {
   it('generate examples', async () => {
     buildSetup();
     await startCreating();
-    console.log(config.layerConfigurations);
     const checkDna = checkUniqGeneratedDna({ noConsole: true });
-    console.log(checkDna);
     const lastConfig =
       config.layerConfigurations[config.layerConfigurations.length - 1];
     expect(checkDna).toEqual(lastConfig.growEditionSizeTo);
